769000346ce6e4f6e4b15d0919e303fbc8a03cd1
Hamish Burke <hamishapps@gmail.com>
2025-08-01 13:55:34 +1200

    maybe fix

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from '../../supabase/types'
 
-const supabaseUrl = import.meta.env.SUPABASE_DATABASE_URL || process.env.SUPABASE_DATABASE_URL;
+const supabaseUrl = process.env.SUPABASE_DATABASE_URL;
+
 const supabaseKey = import.meta.env.SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY;
 
 export const supabase = supabaseUrl && supabaseKey ? createClient<Database>(supabaseUrl, supabaseKey) : null;
