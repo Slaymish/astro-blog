01a08c8e19d94a0144f9014ad0305c158ba4bf7a
Hamish Burke <hamishapps@gmail.com>
2026-01-28 17:55:47 +1300

    feat: disable CDN usage for Sanity client configuration in both dev and production setups

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       projectId: process.env.SANITY_PROJECT_ID || '',
       dataset: process.env.SANITY_DATASET || 'production',
       apiVersion: '2024-01-01',
-      useCdn: true,
+      useCdn: false,
       studioBasePath: '/cms'
     })
   ],
